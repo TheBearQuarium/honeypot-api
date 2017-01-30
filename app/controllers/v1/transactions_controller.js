027fa285a41df3493b5bccb37cd86d1341f7c44c
Connor <cgreyparsons@gmail.com>
2017-01-30 11:17:08 -0600

    Update

--- app/controllers/v1/transactions_controller.js
+++ app/controllers/v1/transactions_controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const stripe = require('stripe')('sk_test_XtAMFVO64j1hd1Fiud3lCVdj');
 const Nodal = require('nodal');
 const Transaction = Nodal.require('app/models/transaction.js');
 
@@ -29,12 +30,23 @@ class V1TransactionsController extends Nodal.Controller {
 
   create() {
 
-    Transaction.create(this.params.body, (err, model) => {
+    const amount = this.params.body.amount;
+    const checking = this.params.body.checking;
+    var context = this;
 
-      this.respond(err || model);
-
+    stripe.charges.create({
+      amount: amount,
+      currency: "usd",
+      customer: checking
     });
 
+    console.log('it happened! WTFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF');
+
+    Transaction.create(context.params.body, (err, model) => {
+      console.log('it happened! WTFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF');
+
+      this.respond(err || model);
+    });
   }
 
   update() {
